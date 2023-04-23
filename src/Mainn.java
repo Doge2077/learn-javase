a0d14c0260e4b2776012e7b88c7ac9f6a5c3a596
Dog2077 <1002302241@qq.com>
2023-04-23 23:31:17 +0800

    Learn exception.

---
+++ src/Mainn.java
@@ -0,0 +1,14 @@
+public class Mainn {
+    public static void main(String[] args) {
+//        Object object = null;
+//        object.toString();   //这种情况就会出现运行时异常
+
+//        Object object = new Object();
+//        Mainn main = (Mainn) object;
+
+        Object[] objects = new Object[Integer.MAX_VALUE];   //这里申请一个超级大数组
+
+    }
+
+
+}
