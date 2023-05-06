0cb85a6ce0792da2d5e317752e7853736db417ea
Dog2077 <1002302241@qq.com>
2023-05-06 18:03:20 +0800

    Finished learn thread

--- src/Main.java
+++ src/Main.java
@@ -464,8 +464,140 @@ public class Main {
 //            e.printStackTrace();
 //        }
 
-        
+//        Thread t1 = new Thread(() -> {
+//            for (int i = 0; i < 10000; i++) {
+//                threadValue ++;
+//            }
+//            System.out.println("线程1完成");
+//        });
+//        Thread t2 = new Thread(() -> {
+//            for (int i = 0; i < 10000; i++) {
+//                threadValue ++;
+//            }
+//            System.out.println("线程2完成");
+//        });
+//        t1.start();
+//        t2.start();
+//        Thread.sleep(1000);  //主线程停止1秒，保证两个线程执行完成
+//        System.out.println(threadValue);  // 由于写入冲突导致 threadValue 的值小于等于 20000
 
+//        Thread t1 = new Thread(() -> {
+//            for (int i = 0; i < 10000; i++) {
+//                synchronized (Main.class){  //使用 synchronized 关键字创建同步代码块
+//                    threadValue ++;
+//                }
+//            }
+//            System.out.println("线程1完成");
+//        });
+//        Thread t2 = new Thread(() -> {
+//            for (int i = 0; i < 10000; i++) {
+//                synchronized (Main.class){
+//                    threadValue ++;
+//                }
+//            }
+//            System.out.println("线程2完成");
+//        });
+//        t1.start();
+//        t2.start();
+//        Thread.sleep(1000);  //主线程停止1秒，保证两个线程执行完成
+//        System.out.println(threadValue);
+
+//        Object lock = new Object();  // 同一个对象的锁
+//        Thread t1 = new Thread(() -> {
+//            for (int i = 0; i < 10000; i++) {
+//                synchronized (lock){  //使用 synchronized 关键字创建同步代码块
+//                    threadValue ++;
+//                }
+//            }
+//            System.out.println("线程1完成");
+//        });
+//        Thread t2 = new Thread(() -> {
+//            for (int i = 0; i < 10000; i++) {
+//                synchronized (lock){
+//                    threadValue ++;
+//                }
+//            }
+//            System.out.println("线程2完成");
+//        });
+//        t1.start();
+//        t2.start();
+//        Thread.sleep(1000);  //主线程停止1秒，保证两个线程执行完成
+//        System.out.println(threadValue);
+
+//        Object lockOne = new Object();
+//        Object lockSecond = new Object();
+//        Thread t1 = new Thread(() -> {
+//            for (int i = 0; i < 10000; i++) {
+//                synchronized (lockOne){  //使用 synchronized 关键字创建同步代码块
+//                    threadValue ++;
+//                }
+//            }
+//            System.out.println("线程1完成");
+//        });
+//        Thread t2 = new Thread(() -> {
+//            for (int i = 0; i < 10000; i++) {
+//                synchronized (lockSecond){
+//                    threadValue ++;
+//                }
+//            }
+//            System.out.println("线程2完成");
+//        });
+//        t1.start();
+//        t2.start();
+//        Thread.sleep(1000);  //主线程停止1秒，保证两个线程执行完成
+//        System.out.println(threadValue);  // 不同对象的锁仍不能同步
+//        Vector<Integer> v = new Vector<>();
+//        for (int i = 0; i < 10; i ++) {
+//            Random r = new Random();
+//            v.add(r.nextInt(1000));
+//        }
+//        Collections.sort(v, ((o1, o2) -> o2 - o1));
+//        System.out.println(v.toString());
+
+        new Thread(Main::threadProductor, "P1").start();
+        new Thread(Main::threadProductor, "P2").start();
+        new Thread(Main::threadConsumer, "C1").start();
+        new Thread(Main::threadConsumer, "C2").start();
+        new Thread(Main::threadConsumer, "C3").start();
+
+    }
+
+    private static final Queue<Object> theradQueue = new LinkedList<>();
+
+    public static void threadProductor() {
+        while (true) {
+            try {
+                Thread.sleep(3000);
+                synchronized (theradQueue) {
+                    String threadName = Thread.currentThread().getName();
+                    System.out.println("Productor " + threadName + " are producting...");
+                    if (theradQueue.offer(new Object())) {
+                        System.out.println("Productor " + threadName + " production finished!!!");
+                    }
+                    theradQueue.notifyAll();
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public static void threadConsumer() {
+        while (true) {
+            try {
+                Thread.sleep(4000);
+                synchronized (theradQueue) {
+                    while (theradQueue.isEmpty()) theradQueue.wait();  // 等待出餐
+                    String threadName = Thread.currentThread().getName();
+                    System.out.println("Consumer " + threadName + " are consuming...");
+                    if (theradQueue.poll() != null) {
+                        System.out.println("Consumer " + threadName + " consumption finished!!!");
+                    }
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     public static void hhh(Action act) {
